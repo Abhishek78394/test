70d4f471525abc9a008178afdae8075ba4622a7a
Abhishek78394 <abhi78394@gmail.com>
2024-05-06 18:53:50 +0530

    second commit

--- script/script.js
+++ script/script.js
@@ -8,3 +8,18 @@ document.getElementById("cross").addEventListener("click", (e) => {
     document.getElementById("navbar").style.display = "none"
   });
   
+  document.querySelectorAll('.mainHeader a').forEach(link => {
+    link.addEventListener('click', (e) => {
+      let targetId = e.target.getAttribute('href');
+      let targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        e.preventDefault(); // Prevent the default navigation behavior
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+      document.getElementById("navbar").classList.remove("navbar");
+      document.getElementById("navbar").style.display = "none";
+    });
+  });
+  
